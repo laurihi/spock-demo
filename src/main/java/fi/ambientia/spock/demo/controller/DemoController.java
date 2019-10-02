6cd8b1bbdd19197dbae46e90d083bc3ce1a95b25
Lauri Hiltunen <lauri.hiltunen@ambientia.fi>
2019-10-02 13:40:31 +0300

    Verify that DemoController is in spring application context

---
+++ src/main/java/fi/ambientia/spock/demo/controller/DemoController.java
@@ -0,0 +1,9 @@
+package fi.ambientia.spock.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+}
--- src/test/groovy/fi/ambientia/spock/demo/LoadContextSpec.groovy
+++ src/test/groovy/fi/ambientia/spock/demo/LoadContextSpec.groovy
@@ -1,13 +1,18 @@
 package fi.ambientia.spock.demo
 
+import fi.ambientia.spock.demo.controller.DemoController
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
 @SpringBootTest
 class LoadContextTest extends Specification {
 
-    def "context loads without exceptions"() {
-        expect: "the context is loaded"
+    @Autowired
+    DemoController demoController
 
+    def "context loads without exceptions"() {
+        expect: "democontroller is added to spring context"
+        demoController
     }
 }

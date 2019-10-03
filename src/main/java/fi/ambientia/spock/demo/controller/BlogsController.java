eff1879455e9765c1a6ce323b1279cfabf59b7bd
Lauri Hiltunen <lauri.hiltunen@ambientia.fi>
2019-10-03 20:21:45 +0300

    Fix profile setup, add swagger

--- src/main/java/fi/ambientia/spock/demo/controller/BlogsController.java
+++ src/main/java/fi/ambientia/spock/demo/controller/BlogsController.java
@@ -2,11 +2,13 @@ package fi.ambientia.spock.demo.controller;
 
 import fi.ambientia.spock.demo.model.PostList;
 import fi.ambientia.spock.demo.service.BlogService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class BlogsController {
 

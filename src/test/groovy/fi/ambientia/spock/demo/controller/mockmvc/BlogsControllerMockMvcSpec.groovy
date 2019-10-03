c7a98efd4b93be592ec23167f03bfb911deaa304
Lauri Hiltunen <lauri.hiltunen@ambientia.fi>
2019-10-03 20:02:42 +0300

    Stupid parameterized tests to showcase speed difference between mockmvc and springboottest. Included the dirtiescontext to emphasize the difference

--- src/test/groovy/fi/ambientia/spock/demo/controller/mockmvc/BlogsControllerMockMvcSpec.groovy
+++ src/test/groovy/fi/ambientia/spock/demo/controller/mockmvc/BlogsControllerMockMvcSpec.groovy
@@ -11,15 +11,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @AutoConfigureMockMvc
 @WebMvcTest(controllers = [BlogsController])
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BlogsControllerMockMvcSpec extends Specification{
 
     def postApiBase = "http://url.to.service"
@@ -69,5 +72,33 @@ class BlogsControllerMockMvcSpec extends Specification{
             result.andExpect(status().isOk())
                 .andReturn()
                 .response.getContentAsString().contains("posts")
+    }
+
+    @Unroll
+    def "Stupid parameterized test run #runNumber to showcase perfomance between SpringBootTest and MockMvcTest"() {
+        given: "api response from blog api contains one blog post"
+            List<Post> resultPosts = [new Post()]
+            ResponseEntity<List< Post>> apiResponse = new ResponseEntity<>(resultPosts, HttpStatus.OK)
+            restTemplate.exchange(postApiBase+"/posts",
+                    HttpMethod.GET, null, _) >> apiResponse
+
+        when: "get all posts endpoint on controller called"
+            def result = mvc.perform(get("/posts"))
+
+        then: "blog post api endpoint /posts is called'"
+            result.andExpect(status().isOk())
+                .andReturn()
+                .response.getContentAsString().contains("posts")
+        where:
+            runNumber   | _
+            1           | _
+            2           | _
+            3           | _
+            4           | _
+            5           | _
+            6           | _
+            7           | _
+            8           | _
+            9           | _
     }
 }
--- src/test/groovy/fi/ambientia/spock/demo/controller/springboot/BlogsControllerSpringBootSpec.groovy
+++ src/test/groovy/fi/ambientia/spock/demo/controller/springboot/BlogsControllerSpringBootSpec.groovy
@@ -1,6 +1,5 @@
 package fi.ambientia.spock.demo.controller.springboot
 
-
 import fi.ambientia.spock.demo.model.Post
 import fi.ambientia.spock.demo.model.PostList
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,17 +10,18 @@ import org.springframework.context.annotation.Import
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(IntegrationTestConfiguration)
 @ActiveProfiles("test")
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BlogsControllerSpringBootSpec extends Specification {
 
     @Autowired
@@ -67,5 +67,32 @@ class BlogsControllerSpringBootSpec extends Specification {
             response.body.posts.size() == 1
     }
 
+    @Unroll
+    def "Stupid parameterized test run #runNumber to showcase perfomance between SpringBootTest and MockMvcTest'"() {
+        given: "postApi returns one blog post"
+            List<Post> resultPosts = [new Post()]
+            ResponseEntity<List< Post>> apiResponse = new ResponseEntity<>(resultPosts, HttpStatus.OK)
+            mockRestTemplate.exchange(postApiBase+"/posts",
+                    HttpMethod.GET, null, _) >> apiResponse
+
+        when: "get all posts endpoint on controller called"
+            def response = restTemplate.getForEntity('/posts', PostList)
+
+        then: "response from blogs controller is 200 an contains one post"
+            response.statusCode == HttpStatus.OK
+            response.body.posts
+            response.body.posts.size() == 1
+        where:
+            runNumber   | _
+            1           | _
+            2           | _
+            3           | _
+            4           | _
+            5           | _
+            6           | _
+            7           | _
+            8           | _
+            9           | _
+    }
 
 }
